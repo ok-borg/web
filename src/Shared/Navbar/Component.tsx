bed9c4e987ea5e51c2dbdb2168157ebd4c33a40a
Anas AIT ALI <aitali.anas@gmail.com>
2017-03-05 11:22:45 +0000

    Submit search on enter and onclick

--- src/Shared/Navbar/Component.tsx
+++ src/Shared/Navbar/Component.tsx
@@ -3,43 +3,44 @@ import {SearchInput} from '../SearchInput/Component';
 import {Logo} from '../Logo/Component';
 
 interface IProps {
-    showSearch?: boolean
-    inputOnChange?: (e: Event) => void
-    inputOnSearch?: (e: Event) => void
-    inputSearchValue?: string
+  showSearch?: boolean
+  inputOnChange?: (e: Event) => void
+  inputOnSearch?: (e: Event) => void
+  inputSearchValue?: string
 }
 
 export const Navbar = (props: IProps) => {
-    const RenderLogo = () => {
-        return props.showSearch ? <Logo className='nav-sive left'/> : '';
-    };
+  const RenderLogo = () => {
+    return props.showSearch ? <Logo className='nav-sive left'/> : '';
+  };
 
-    return (
-        <div id="navbar" className="navbar-fixed row">
-            <nav className={props.showSearch ? 'visible-bg' : ''}>
-                <div className="nav-wrapper">
-                    {RenderLogo()}
-                    {props.showSearch &&
-                    <SearchInput
-                        value={props.inputSearchValue}
-                        onChange={props.inputOnChange}
-                        className='left col s5'/>
-                    }
-                    {props.showSearch &&
-                    <button
-                        type="submit"
-                        name="search"
-                        className="search-button"
-                        onClick={props.inputOnSearch}
-                    >Search</button>
-                    }
-                    <ul id="nav-mobile" className="right">
-                        <li>
-                            <a href="#"><img src="http://materializecss.com/images/yuna.jpg" className="circle"/></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+    <div id="navbar" className="navbar-fixed row">
+      <nav className={props.showSearch ? 'visible-bg' : ''}>
+        <div className="nav-wrapper">
+          {RenderLogo()}
+          <form onSubmit={props.inputOnSearch}>
+            {props.showSearch &&
+            <SearchInput
+              value={props.inputSearchValue}
+              onChange={props.inputOnChange}
+              className='left col s5'/>
+            }
+            {props.showSearch &&
+            <button
+              type="submit"
+              name="search"
+              className="search-button"
+            >Search</button>
+            }
+          </form>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a href="#"><img src="http://materializecss.com/images/yuna.jpg" className="circle"/></a>
+            </li>
+          </ul>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
